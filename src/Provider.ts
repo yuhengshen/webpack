af44505040ccfb34d595a2db87a9450f6953ea8e
hengshenyu <yuhengshen@qq.com>
2020-12-18 08:42:52 +0800

    format code

--- src/Provider.ts
+++ src/Provider.ts
@@ -1,74 +1,109 @@
-import { CancellationToken, DefinitionProvider, extensions, Location, LocationLink, Position, ProviderResult, TextDocument, Uri, window } from "vscode";
+import {
+  CancellationToken,
+  DefinitionProvider,
+  extensions,
+  Location,
+  LocationLink,
+  Position,
+  ProviderResult,
+  TextDocument,
+  Uri,
+  window,
+} from "vscode";
 import * as path from "path";
-import * as fs from 'fs';
+import * as fs from "fs";
 export class JumpDefinitionProvider implements DefinitionProvider {
-    private extensions: string[];
-    private alias: { [key: string]: string };
-    constructor({ extensions = [], alias = {} }: { extensions: string[], alias: { [key: string]: any } }) {
-        this.extensions = extensions;
-        this.alias = alias;
+  private extensions: string[];
+  private alias: { [key: string]: string };
+  constructor({
+    extensions = [],
+    alias = {},
+  }: {
+    extensions: string[];
+    alias: { [key: string]: any };
+  }) {
+    this.extensions = extensions;
+    this.alias = alias;
+  }
+  provideDefinition(
+    document: TextDocument,
+    position: Position,
+    token: CancellationToken
+  ): ProviderResult<Location | Location[] | LocationLink[]> {
+    const wordPosition = document.getWordRangeAtPosition(
+      position,
+      /('|").+(\\|\/).+('|")/
+    );
+    if (!wordPosition) {
+      // use vscode provider
+      return;
     }
-    provideDefinition(document: TextDocument, position: Position, token: CancellationToken): ProviderResult<Location | Location[] | LocationLink[]> {
-        const wordPosition = document.getWordRangeAtPosition(position, /('|").+(\\|\/).+('|")/);
-        if (!wordPosition) {
-            // use vscode provider
-            return;
-        };
-        let word = document.getText(wordPosition).slice(1, -1);
+    let word = document.getText(wordPosition).slice(1, -1);
 
-        // replace alias
-        const aliasKeys = Object.keys(this.alias);
-        let usedAlias = false;
-        for (let i = 0; i < aliasKeys.length; i++) {
-            if (word.startsWith(aliasKeys[i])) {
-                word = word.replace(aliasKeys[i], this.alias[aliasKeys[i]]);
-                usedAlias = true;
-                break;
-            }
-        }
-        // replace extensions
-        const dir = path.dirname(document.uri.path);
-        const reverse = (extensionsIndex: number): string | null => {
-            const relativePath = extensionsIndex === -1 ? word : `${word}${this.extensions[extensionsIndex]}`;
-            const tempPath = (path.resolve(dir, relativePath));
-            try {
-                const state = fs.statSync(tempPath);
-                if (state.isFile()) {
-                    // vetur has impl these provider
-                    if (!usedAlias && ['.js', '.jsx', '.ts', '.tsx'].includes(this.extensions[extensionsIndex])) {
-                        return null;
-                    }
-                    return tempPath;
-                };
-                if (extensionsIndex === -1) {
-                    const arr = ['.js', '.jsx', '.ts', '.tsx', ...this.extensions];
-                    // exit tempPath.js/tempPath.jsx/tempPath.jsx...
-                    for (let i = 0; i < arr.length; i++) {
-                        const isExit = fs.existsSync(`${tempPath}${arr[i]}`);
-                        if (isExit) {
-                            return `${tempPath}${arr[i]}`;
-                        }
-                    }
-                    // exit tempPath/index.js/tempPath/index.jsx/tempPath/index.jsx...
-                    if (state.isDirectory()) {
-                        for (let i = 0; i < arr.length; i++) {
-                            const isExit = fs.existsSync(path.resolve(tempPath, `./index${arr[i]}`));
-                            if (isExit) {
-                                return path.resolve(tempPath, `./index${arr[i]}`);
-                            }
-                        }
-                    }
-                }
-            } catch {
-                // not exit tempPath
-                if (extensionsIndex < this.extensions.length) return reverse(extensionsIndex + 1);
-             }
+    // replace alias
+    const aliasKeys = Object.keys(this.alias);
+    let usedAlias = false;
+    for (let i = 0; i < aliasKeys.length; i++) {
+      if (word.startsWith(aliasKeys[i])) {
+        word = word.replace(aliasKeys[i], this.alias[aliasKeys[i]]);
+        usedAlias = true;
+        break;
+      }
+    }
+    // replace extensions
+    const dir = path.dirname(document.uri.path);
+    const reverse = (extensionsIndex: number): string | null => {
+      const relativePath =
+        extensionsIndex === -1
+          ? word
+          : `${word}${this.extensions[extensionsIndex]}`;
+      const tempPath = path.resolve(dir, relativePath);
+      try {
+        const state = fs.statSync(tempPath);
+        if (state.isFile()) {
+          // vetur has impl these provider
+          if (
+            !usedAlias &&
+            [".js", ".jsx", ".ts", ".tsx"].includes(
+              this.extensions[extensionsIndex]
+            )
+          ) {
             return null;
-        };
-        const filepath = reverse(-1);
-
-        if (filepath !== null) {
-            return new Location(Uri.parse(filepath), new Position(0, 0));
+          }
+          return tempPath;
         }
+        if (extensionsIndex === -1) {
+          const arr = [".js", ".jsx", ".ts", ".tsx", ...this.extensions];
+          // exit tempPath.js/tempPath.jsx/tempPath.jsx...
+          for (let i = 0; i < arr.length; i++) {
+            const isExit = fs.existsSync(`${tempPath}${arr[i]}`);
+            if (isExit) {
+              return `${tempPath}${arr[i]}`;
+            }
+          }
+          // exit tempPath/index.js/tempPath/index.jsx/tempPath/index.jsx...
+          if (state.isDirectory()) {
+            for (let i = 0; i < arr.length; i++) {
+              const isExit = fs.existsSync(
+                path.resolve(tempPath, `./index${arr[i]}`)
+              );
+              if (isExit) {
+                return path.resolve(tempPath, `./index${arr[i]}`);
+              }
+            }
+          }
+        }
+      } catch {
+        // not exit tempPath
+        if (extensionsIndex < this.extensions.length)
+          return reverse(extensionsIndex + 1);
+      }
+      return null;
+    };
+    const filepath = reverse(-1);
+
+    if (filepath !== null) {
+      return new Location(Uri.parse(filepath), new Position(0, 0));
     }
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,82 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as path from "path";
 import * as fs from "fs";
 import { JumpDefinitionProvider } from "./Provider";
 
 const showError = vscode.window.showErrorMessage;
 const showInfo = vscode.window.showInformationMessage;
 const showWarn = vscode.window.showWarningMessage;
-
+// 工作区目录可能有多个
+// todo:: 处理多工作区
+const projectDir = vscode.workspace.workspaceFolders?.[0];
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand("extension.path", async () => {
-		const uri = await vscode.window.showOpenDialog({
-			canSelectFolders: false,
-			canSelectMany: false,
-			title: "select webpack configuration file"
-		});
-		if (uri && uri.length > 0) {
-			context.workspaceState.update("webpackConfigPath", uri[0].path);
-			// reload file content
-			main(context);
-		}
-	});
-	context.subscriptions.push(disposable);
-	main(context);
+  if (!projectDir) return;
+  const disposable = vscode.commands.registerCommand(
+    "extension.path",
+    async () => {
+      const uri = await vscode.window.showOpenDialog({
+        canSelectFolders: false,
+        canSelectMany: false,
+        title: "select webpack configuration file",
+      });
+      if (uri && uri.length > 0) {
+        context.workspaceState.update("webpackConfigPath", uri[0].path);
+        // reload file content
+        main(context);
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
+  main(context);
 }
 
 let definitionProvider: vscode.Disposable | undefined;
 let filewatcher: vscode.FileSystemWatcher | undefined;
 
 export function deactivate() {
-	definitionProvider?.dispose();
-	filewatcher?.dispose();
- }
+  definitionProvider?.dispose();
+  filewatcher?.dispose();
+}
 
 async function main(context: vscode.ExtensionContext) {
-	const webpackConfigPath = context.workspaceState.get("webpackConfigPath") as string || "./webpack.config.js";
-	const rootPath = vscode.workspace.workspaceFolders![0].uri.path;
-	const fullPath = path.resolve(rootPath, webpackConfigPath);
-	if (!fs.existsSync(fullPath)) {
-		return [];
-	}
-	let webpackConfig;
-	try {
-		delete require.cache[require.resolve(path.resolve(rootPath, webpackConfigPath))];
-		webpackConfig = require(path.resolve(rootPath, webpackConfigPath));
-	} catch (e) {
-		showError(`load webpack config error: ${e}`);
-		return [];
-	}
-	const alias = webpackConfig.resolve?.alias || {};
-	const extensions = webpackConfig.resolve?.extensions || [];
-	definitionProvider?.dispose();
-	definitionProvider = vscode.languages.registerDefinitionProvider({
-		scheme: "file",
-		pattern: "**/*.{js,jsx,ts,tsx,vue}"
-	}, new JumpDefinitionProvider({ extensions, alias }));
-	// configuration file watcher
-	filewatcher?.dispose();
-	filewatcher = vscode.workspace.createFileSystemWatcher(fullPath, true, false, true);
-	filewatcher.onDidChange(() => main(context));
+  const webpackConfigPath =
+    (context.workspaceState.get("webpackConfigPath") as string) ||
+    "./webpack.config.js";
+  const rootPath = vscode.workspace.workspaceFolders![0].uri.path;
+  const fullPath = path.resolve(rootPath, webpackConfigPath);
+  if (!fs.existsSync(fullPath)) {
+    return [];
+  }
+  let webpackConfig;
+  try {
+    delete require.cache[
+      require.resolve(path.resolve(rootPath, webpackConfigPath))
+    ];
+    // process cwd
+    webpackConfig = require(path.resolve(rootPath, webpackConfigPath));
+  } catch (e) {
+    showError(`load webpack config error: ${e}`);
+    return [];
+  }
+  const alias = webpackConfig.resolve?.alias || {};
+  const extensions = webpackConfig.resolve?.extensions || [];
+  definitionProvider?.dispose();
+  definitionProvider = vscode.languages.registerDefinitionProvider(
+    {
+      scheme: "file",
+      pattern: "**/*.{js,jsx,ts,tsx,vue}",
+    },
+    new JumpDefinitionProvider({ extensions, alias })
+  );
+  // configuration file watcher
+  filewatcher?.dispose();
+  filewatcher = vscode.workspace.createFileSystemWatcher(
+    fullPath,
+    true,
+    false,
+    true
+  );
+  filewatcher.onDidChange(() => main(context));
 
-	showInfo("webpack helper has been enabled");
+  showInfo("webpack helper has been enabled");
 }
